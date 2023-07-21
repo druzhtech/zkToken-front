48197c0177e47ab38847a1c2ba1c6ddc796bc7fd
Sergey <68071039+mysteryon88@users.noreply.github.com>
2023-07-21 22:29:59 +0300

    add siberium test network

--- src/utils/connectors.ts
+++ src/utils/connectors.ts
@@ -1,5 +1,5 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 31337]
+  supportedChainIds: [1, 3, 4, 5, 42, 31337, 111000, 111111],
 });
